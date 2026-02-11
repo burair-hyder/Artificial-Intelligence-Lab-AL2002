29163838470cdc105904f863ffc50eb2df3b7207
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2026-02-11 22:46:23 +0500

    Create question 05.py

---
+++ Lab Work/Lab 03/question 05.py
@@ -0,0 +1,36 @@
+# question 05
+import random
+
+class Environment:
+    def __init__(self):
+        self.actions = ['Play', 'Rest']
+        self.rewards = {'Play': 5, 'Rest': 1}
+
+    def get_reward(self, action):
+        return self.rewards[action]
+
+class Agent:
+    def __init__(self, environment):
+        self.env = environment
+        self.q_table = {action: 0 for action in self.env.actions}
+        self.alpha = 0.5
+
+    def choose_action(self):
+        return random.choice(self.env.actions)
+
+    def update_q(self, action, reward):
+        self.q_table[action] = self.q_table[action] + self.alpha * (reward - self.q_table[action])
+
+def run_agent(iterations):
+    env = Environment()
+    agent = Agent(env)
+    for step in range(1, iterations + 1):
+        action = agent.choose_action()
+        reward = env.get_reward(action)
+        print("Step " ,step , ": Action ", action , " Reward " , reward)
+        agent.update_q(action, reward)
+        if step % 5 == 0: # priting q values after half way
+            print("Q-table Updated")
+            print("Q-values: " + str(agent.q_table))
+
+run_agent(10)
